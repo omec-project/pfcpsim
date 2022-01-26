f5e243314609cd21484b2a996893ea27034962fb
tosinski <tomasz@opennetworking.org>
2022-01-26 10:28:18 +0100

    Migrate pfcpsim package

---
+++ cmd/pfcpsim-client/main.go
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 Open Networking Foundation
+
+package main
+
+func main() {
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/omec-project/pfcpsim
+
+go 1.16
+
+require github.com/wmnsk/go-pfcp v0.0.14
