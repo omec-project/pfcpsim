199b253656b11b53a4a9720868d531075298ced9
Emanuele Gallone <emanuele@opennetworking.org>
2022-02-01 11:20:33 +0100

    Add first unit test

---
+++ pkg/pfcpsim/session/far_builder_test.go
@@ -0,0 +1,36 @@
+package session
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFARBuilderShouldPanic(t *testing.T) {
+
+	assert.Panics(t, func() {
+		NewFARBuilder().
+			WithMethod(Create).
+			WithAction(ActionDrop).
+			BuildFAR()
+	})
+
+	// Providing partial parameters (DownlinkIP without providing TEID)
+	assert.Panics(t, func() {
+		NewFARBuilder().WithMethod(Create).
+			WithID(1).
+			WithAction(ActionDrop).
+			WithDownlinkIP("10.0.0.1").
+			BuildFAR()
+	})
+
+	// Providing partial parameters (TEID without providing DownlinkIP)
+	assert.Panics(t, func() {
+		NewFARBuilder().WithMethod(Create).
+			WithID(2).
+			WithAction(ActionDrop).
+			WithTEID(100).
+			BuildFAR()
+	})
+
+}
