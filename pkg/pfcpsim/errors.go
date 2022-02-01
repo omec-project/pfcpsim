4e51895ac399344bb79f60c79e549749e54abaaa
Emanuele Gallone <emanuele@opennetworking.org>
2022-02-01 10:45:10 +0100

    Add custom error types

--- pkg/pfcpsim/errors.go
+++ pkg/pfcpsim/errors.go
@@ -1,10 +1,40 @@
 package pfcpsim
 
-import "errors"
+import "fmt"
 
-// TODO create custom error types
+type pfcpSimError struct {
+	message string
+	error   error
+}
 
-var InvalidCauseErr = errors.New("InvalidCause")
-var TimeoutOccurredErr = errors.New("TimeoutOccurred")
-var InvalidResponseErr = errors.New("InvalidResponse")
-var AssociationInactiveErr = errors.New("AssociationInactive")
+func (e *pfcpSimError) Error() string {
+	return fmt.Sprintf("Message: %v, Error: %v", e.message, e.error)
+}
+
+func NewInvalidCauseError(err error) *pfcpSimError {
+	return &pfcpSimError{
+		message: "Invalid Cause from response",
+		error:   err,
+	}
+}
+
+func NewAssociationInactiveError(err error) *pfcpSimError {
+	return &pfcpSimError{
+		message: "Could not complete operation. Association is not active",
+		error:   err,
+	}
+}
+
+func NewTimeoutExpiredError(err error) *pfcpSimError {
+	return &pfcpSimError{
+		message: "Timeout has expired",
+		error:   err,
+	}
+}
+
+func NewInvalidResponseError(err error) *pfcpSimError {
+	return &pfcpSimError{
+		message: "Invalid response received",
+		error:   err,
+	}
+}
