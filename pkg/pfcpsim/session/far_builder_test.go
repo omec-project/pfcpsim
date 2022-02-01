0555a102e7594cc008b2659bd7200ae406c50611
Emanuele Gallone <emanuele@opennetworking.org>
2022-02-01 13:30:06 +0100

    Add expected field in test struct

--- pkg/pfcpsim/session/far_builder_test.go
+++ pkg/pfcpsim/session/far_builder_test.go
@@ -4,13 +4,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/wmnsk/go-pfcp/ie"
 )
 
 func TestFARBuilderShouldPanic(t *testing.T) {
 	type testCase struct {
-		input *farBuilder
-		expected *ie.IE
+		input       *farBuilder
+		expected    *farBuilder
 		description string
 	}
 
@@ -19,30 +18,42 @@ func TestFARBuilderShouldPanic(t *testing.T) {
 			input: NewFARBuilder().
 				WithMethod(Create).
 				WithAction(ActionDrop),
-			expected: nil,
+			expected: &farBuilder{
+				method:      Create,
+				applyAction: ActionDrop,
+			},
 			description: "Invalid FAR: No ID provided",
 		},
 		{
 			input: NewFARBuilder().
 				WithMethod(Create).
-			WithID(2).
-			WithAction(ActionDrop).
-			WithTEID(100),
+				WithID(2).
+				WithAction(ActionDrop).
+				WithTEID(100),
 
-			expected: nil,
+			expected: &farBuilder{
+				farID:       2,
+				applyAction: ActionDrop,
+				teid:        100,
+			},
 			description: "Invalid FAR: Providing TEID without DownlinkIP",
 		},
 		{
 			input: NewFARBuilder().WithMethod(Create).
 				WithID(1).
-				WithAction(ActionDrop).
+				WithAction(ActionForward).
 				WithDownlinkIP("10.0.0.1"),
-			expected: nil,
+			expected: &farBuilder{
+				farID:       1,
+				applyAction: ActionForward,
+				downlinkIP:  "10.0.0.1",
+			},
 			description: "Invalid FAR: Providing DownlinkIP without TEID",
 		},
 	} {
 		t.Run(scenario.description, func(t *testing.T) {
-			assert.Panics(t, func() {scenario.input.BuildFAR()} )
+			assert.Panics(t, func() { scenario.input.BuildFAR() })
+			assert.Equal(t, scenario.input, scenario.expected)
 		})
 	}
 }
