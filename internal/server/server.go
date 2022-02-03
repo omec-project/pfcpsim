1cd134a01e8482aa75124749bc15acac2d9e5f22
Emanuele Gallone <emanuele@opennetworking.org>
2022-02-03 14:21:34 +0100

    Refactor

--- internal/server/server.go
+++ internal/server/server.go
@@ -8,7 +8,7 @@ package server
 import (
 	"context"
 
-	"github.com/omec-project/pfcpsim/api"
+	pfcpsimctl "github.com/omec-project/pfcpsim/api"
 )
 
 type PFCPSimServer struct {
@@ -29,27 +29,27 @@ func (P PFCPSimServer) StartgRPCServer(ctx context.Context, empty *pfcpsimctl.Em
 	panic("implement me")
 }
 
-func (P PFCPSimServer) Associate(ctx context.Context, request *pfcpsimctl.AssociateRequest) (*pfcpsimctl.Response, error) {
+func (P PFCPSimServer) Associate(ctx context.Context, empty *pfcpsimctl.Empty) (*pfcpsimctl.Response, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (P PFCPSimServer) Disassociate(ctx context.Context, request *pfcpsimctl.DisassociateRequest) (*pfcpsimctl.Response, error) {
+func (P PFCPSimServer) Disassociate(ctx context.Context, empty *pfcpsimctl.Empty) (*pfcpsimctl.Response, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (P PFCPSimServer) CreateSession(ctx context.Context, request *pfcpsimctl.CreateSessionRequest) (*pfcpsimctl.Response, error) {
+func (P PFCPSimServer) CreateSession(ctx context.Context, empty *pfcpsimctl.Empty) (*pfcpsimctl.Response, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (P PFCPSimServer) ModifySession(ctx context.Context, request *pfcpsimctl.ModifySessionRequest) (*pfcpsimctl.Response, error) {
+func (P PFCPSimServer) ModifySession(ctx context.Context, empty *pfcpsimctl.Empty) (*pfcpsimctl.Response, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (P PFCPSimServer) DeleteSession(ctx context.Context, request *pfcpsimctl.DeleteSessionRequest) (*pfcpsimctl.Response, error) {
+func (P PFCPSimServer) DeleteSession(ctx context.Context, empty *pfcpsimctl.Empty) (*pfcpsimctl.Response, error) {
 	//TODO implement me
 	panic("implement me")
 }
