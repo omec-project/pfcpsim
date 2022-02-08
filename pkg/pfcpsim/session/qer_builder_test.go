ed88ec96fc677d1bcc0e75432223d9d007ed3773
Emanuele Gallone <emanuele@opennetworking.org>
2022-02-08 10:50:18 +0100

    Add QER building tests

--- pkg/pfcpsim/session/qer_builder_test.go
+++ pkg/pfcpsim/session/qer_builder_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/wmnsk/go-pfcp/ie"
 )
 
 func TestQERBuilderShouldPanic(t *testing.T) {
@@ -44,6 +45,66 @@ func TestQERBuilderShouldPanic(t *testing.T) {
 		t.Run(scenario.description, func(t *testing.T) {
 			assert.Panics(t, func() { scenario.input.Build() })
 			assert.Equal(t, scenario.input, scenario.expected)
+		})
+	}
+}
+
+func TestQERBuilder(t *testing.T) {
+	type testCase struct {
+		input       *qerBuilder
+		expected    *ie.IE
+		description string
+	}
+
+	for _, scenario := range []testCase{
+		{
+			input: NewQERBuilder().
+				WithID(1).
+				WithMethod(Create).
+				WithQFI(2),
+			expected: ie.NewCreateQER(
+				ie.NewQERID(1),
+				ie.NewQFI(2),
+				ie.NewGateStatus(0, 0),
+				ie.NewMBR(0, 0),
+				ie.NewGBR(0, 0),
+			),
+			description: "Valid Create QER",
+		},
+		{
+			input: NewQERBuilder().
+				WithID(1).
+				WithMethod(Update).
+				WithQFI(2),
+			expected: ie.NewUpdateQER(
+				ie.NewQERID(1),
+				ie.NewQFI(2),
+				ie.NewGateStatus(0, 0),
+				ie.NewMBR(0, 0),
+				ie.NewGBR(0, 0),
+			),
+			description: "Valid Update QER",
+		},
+		{
+			input: NewQERBuilder().
+				WithID(1).
+				WithMethod(Delete).
+				WithQFI(2),
+			expected: ie.NewRemoveQER(
+				ie.NewCreateQER(
+					ie.NewQERID(1),
+					ie.NewQFI(2),
+					ie.NewGateStatus(0, 0),
+					ie.NewMBR(0, 0),
+					ie.NewGBR(0, 0),
+				),
+			),
+			description: "Valid Delete QER",
+		},
+	} {
+		t.Run(scenario.description, func(t *testing.T) {
+			assert.NotPanics(t, func() { _ = scenario.input.Build() })
+			assert.Equal(t, scenario.input.Build(), scenario.expected)
 		})
 	}
 }
